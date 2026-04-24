8fcb2cc9b66eb560e6432d797fddcb64849e6776
LindaChocolatina <lindasioc@gmail.com>
2026-04-24 08:03:44 -0500

    Fase 3: Crear servicios faltantes (carrito, blog, newsletter, orden)

---
+++ app/services/newsletter_service.py
@@ -0,0 +1,13 @@
+"""
+Servicio de newsletter
+Stub para el MVP
+"""
+class NewsletterService:
+    def __init__(self, repository=None):
+        self.repository = repository
+        
+    def suscribir(self, usuario_id, artista_id):
+        pass
+        
+    def desuscribir(self, usuario_id, artista_id):
+        pass
---
+++ app/services/orden_service.py
@@ -0,0 +1,13 @@
+"""
+Servicio de ordenes
+Stub para el MVP
+"""
+class OrdenService:
+    def __init__(self, repository=None):
+        self.repository = repository
+        
+    def crear_orden(self, data):
+        return None
+        
+    def get_by_cliente(self, cliente_id):
+        return []
